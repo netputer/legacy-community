1aa06ada1c512c107a405813b76a0d88ff13d07a
NetPuter Lin <netputer@wandoujia.com>
2014-09-19 11:30:45 +0800

    chore: Move `$scope` to 'this'

--- app/javascripts/group/PostController.js
+++ app/javascripts/group/PostController.js
@@ -1,43 +1,46 @@
 define(function () {
     var PostController = function ($scope, $routeParams, GroupService) {
-        $scope.message = '';
-        $scope.pictures = [];
-        // $scope.pictures = [
+        var scope = this;
+
+        scope.message = '';
+        scope.pictures = [];
+        // scope.pictures = [
         //     '9852e745981746eda5cf1f61d3ac5208_596_596.jpeg',
         //     '60b7e8e9c5d4e572662536afc4e26dc8_596_441.jpeg'
         // ];
 
-        $scope.uploadBefore = function () {
+        scope.uploadBefore = function () {
             console.log('scope before');
         };
 
-        $scope.uploadProgress = function (e, data) {
+        scope.uploadProgress = function (e, data) {
             console.log('scope progress - e', e);
             console.log('scope progress - data', data);
         };
 
-        $scope.uploadDone = function (e, data) {
+        scope.uploadDone = function (e, data) {
             console.log('scope done - e', e);
             console.log('scope done - data', data);
 
             var result = data.result;
 
             if (result.code === 0) {
-                $scope.pictures.push(result.msg);
-                $scope.$apply();
+                $scope.$apply(function () {
+                    scope.pictures.push(result.msg);
+                });
             }
         };
 
-        $scope.uploadFail = function (e, data) {
+        scope.uploadFail = function (e, data) {
             console.log('scope fail - e', e);
             console.log('scope fail - data', data);
         };
 
-        $scope.postTopic = function () {
+        scope.postTopic = function () {
             GroupService.postTopic({
                 groupId: $routeParams.id,
-                message: $scope.message,
-                pictures: $scope.pictures
+                message: scope.message,
+                pictures: scope.pictures
             }).then(function (xhr) {
                 console.log(xhr);
             });
